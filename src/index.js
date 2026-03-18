3e7fc3baa83de010654102654efdc473dad93ef6
Pavel <yashchenkodev@gmail.com>
2026-03-18 22:50:41 +0200

    feat: add ip location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('123');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+ipInput.addEventListener('keydown', handleKey);
+btn.addEventListener('click', getData);
+
+function getData() {
+  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_FF8gtn6mJwSUjHXWQpsjShtUPYryJ&ipAddress=${ipInput.value}`)
+    .then(response => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
