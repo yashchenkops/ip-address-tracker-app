5e5e322ebe98f4d12cf2ca21eee4549659dd7296
Pavel <yashchenkodev@gmail.com>
2026-03-19 00:10:16 +0200

    feat: add ip validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIp } from './helpers';
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
@@ -5,9 +7,11 @@ ipInput.addEventListener('keydown', handleKey);
 btn.addEventListener('click', getData);
 
 function getData() {
-  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_FF8gtn6mJwSUjHXWQpsjShtUPYryJ&ipAddress=${ipInput.value}`)
-    .then(response => response.json())
-    .then(console.log);
+  if (validateIp(ipInput.value)) {
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_FF8gtn6mJwSUjHXWQpsjShtUPYryJ&ipAddress=${ipInput.value}`)
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
